11e795020b7491c217eae053192f303ddb34a34e
rwacarter <richardcarter87@gmail.com>
2018-09-27 14:07:31 +0100

    feat: allow summary list item actions

--- components/summarylist/summarylistitem/index.js
+++ components/summarylist/summarylistitem/index.js
@@ -4,12 +4,27 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
+const parseDef = (def) => {
+    if(!def) {
+        return '(None)';
+    }
+    if(!Array.isArray(def)) {
+        return def;
+    }
+    return (
+        <span>
+            <span>{def[0]}</span>
+            &nbsp;&nbsp;<a onClick={() => def[2]()}>({def[1]})</a>
+        </span>
+    );
+};
+
 const SummaryListItem = props => (
     <div className={styles.root}>
         <div className={styles.inner}>
             <dt className={styles.term}>{props.term}</dt>
             <dd className={classNames(styles.def, props.def === null ? styles.defIsDisabled : null)}>
-                {props.def || '(None)'}
+                {parseDef(props.def)}
             </dd>
         </div>
     </div>
@@ -17,7 +32,7 @@ const SummaryListItem = props => (
 
 SummaryListItem.propTypes = {
     term: PropTypes.string.isRequired,
-    def: PropTypes.string,
+    def: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
 };
 
 SummaryListItem.defaultProps = {
