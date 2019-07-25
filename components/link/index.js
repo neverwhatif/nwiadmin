c50c92f6d300a8beedf20b82458a3b04371311bd
rwacarter <richardcarter87@gmail.com>
2019-07-25 15:24:46 +0100

    feat: allow external urls in links

--- components/link/index.js
+++ components/link/index.js
@@ -2,22 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 
-const Link = ({ isExternal, ...props }) => (
-    isExternal ? (
-        <a href={props.to} target="_blank" rel="noopener noreferrer" {...props}>{props.children}</a>
-    ) : (
-        <RouterLink {...props}>{props.children}</RouterLink>
-    )
-);
+const Link = ({ to, isExternal, children, ...otherProps }) => {
+    const newWindowParams = isExternal ? { target: '_blank', rel: 'noopener noreferrer' } : {};
+
+    if (to.match(/^http/u) || to.match(/^mailto:/u) || to.match(/^tel:/u)) {
+        return (
+            <a href={to} {...otherProps} {...newWindowParams}>
+                {children}
+            </a>
+        );
+    }
+
+    return (
+        <RouterLink to={to} {...otherProps} {...newWindowParams}>
+            {children}
+        </RouterLink>
+    );
+};
 
 Link.propTypes = {
-    className: PropTypes.string,
-    to: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
+    to: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})])),
+    ]).isRequired,
+    className: PropTypes.string,
+    isNewWindow: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 
 Link.defaultProps = {
+    isNewWindow: false,
     className: null,
+    onClick: () => null,
 };
 
 export default Link;
