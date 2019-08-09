814c00fe24601e8c5d75d83d46b7f042d963ef1d
rwacarter <richardcarter87@gmail.com>
2019-08-09 10:10:06 +0100

    feat: allow summary list to be wrapped

--- components/summarylist/index.js
+++ components/summarylist/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import SummaryListItem from './summarylistitem';
 
 import styles from './styles.scss';
 
 const SummaryList = props => (
-    <dl className={styles.root}>
+    <dl className={classNames(styles.root, props.isWrapped ? styles.rootWrapped : null)}>
         {Object.entries(props.data).map((item, index) => <SummaryListItem key={item[0]} term={item[0]} def={item[1]} isLarge={props.isLarge} isHighlighted={props.highlighted.indexOf(index) > -1} />)}
     </dl>
 );
@@ -14,10 +15,12 @@ const SummaryList = props => (
 SummaryList.propTypes = {
     data: PropTypes.shape({}).isRequired,
     highlighted: PropTypes.array,
+    isWrapped: PropTypes.bool,
 };
 
 SummaryList.defaultProps = {
     highlighted: [],
+    isWrapped: false,
 }
 
 export default SummaryList;
