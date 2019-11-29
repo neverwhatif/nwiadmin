1214216f9d891e725795c45e953e9b7ca71253a8
rwacarter <richardcarter87@gmail.com>
2019-11-29 16:15:53 +0000

    fix: allow null in summary list

--- components/summarylist/index.js
+++ components/summarylist/index.js
@@ -6,11 +6,17 @@ import SummaryListItem from './summarylistitem';
 
 import styles from './styles.scss';
 
-const SummaryList = props => (
-    <dl className={classNames(styles.root, props.isWrapped ? styles.rootWrapped : null)}>
-        {Object.entries(props.data).map((item, index) => <SummaryListItem key={item[0]} term={item[0]} def={item[1]} isLarge={props.isLarge} isHighlighted={props.highlighted.indexOf(index) > -1} />)}
-    </dl>
-);
+const SummaryList = (props) => {
+    if(!props.data) {
+        return null;
+    }
+
+    return (
+        <dl className={classNames(styles.root, props.isWrapped ? styles.rootWrapped : null)}>
+            {Object.entries(props.data).map((item, index) => <SummaryListItem key={item[0]} term={item[0]} def={item[1]} isLarge={props.isLarge} isHighlighted={props.highlighted.indexOf(index) > -1} />)}
+        </dl>
+    );
+};
 
 SummaryList.propTypes = {
     data: PropTypes.shape({}).isRequired,
