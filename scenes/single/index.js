68501cb9f0d669222ab32bfb07e6de285d530e20
rwacarter <richardcarter87@gmail.com>
2019-11-29 16:16:15 +0000

    feat: scene without remote

--- scenes/single/index.js
+++ scenes/single/index.js
@@ -3,6 +3,7 @@ import { withPath } from 'nwiadmin/utility/proptypes';
 
 import ConnectedScene from 'nwiadmin/components/connectedscene';
 import NestedSwitch from 'nwiadmin/components/nestedswitch';
+import Scene from 'nwiadmin/components/scene';
 
 class SingleScene extends Component {
     constructor(props) {
@@ -14,6 +15,16 @@ class SingleScene extends Component {
     }
 
     render() {
+        if(!this.state.remote) {
+            const basePath = this.props.basePath;
+
+            return (
+                <Scene basePath={basePath} title={this.state.title} tabs={this.state.nav.single}>
+                    <NestedSwitch basePath={basePath} routes={this.state.nav.single} />
+                </Scene>
+            );
+        }
+
         const basePath = `${this.props.basePath}/${this.props.match.params.id}`;
 
         return (
