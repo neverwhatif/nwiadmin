86a50a1eb39b03269c07f7e2a18ed29c383712bb
rwacarter <richardcarter87@gmail.com>
2020-02-28 10:23:39 +0000

    fix: meable usecontext

--- services/me/meable.js
+++ services/me/meable.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
+import { useAppContext } from 'nwiadmin/services/context';
+
 const Meable = (ComponentToWrap) => {
     console.warn('Meable is deprecated, and will be removed in a future version');
 
-    const MeableComponent = (props, context) => (
-        <ComponentToWrap {...props} me={context.me || null} />
-    );
-
-    MeableComponent.contextTypes = {
-        me: PropTypes.object,
-    };
+    const MeableComponent = (props) => {
+        const appContext = useAppContext();
+        return (<ComponentToWrap {...props} me={appContext.me || null} />);
+    }
 
     return MeableComponent;
 };
